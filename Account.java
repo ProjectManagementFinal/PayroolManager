abad2c1625c3192e641bae015a79ed461491583f
Adam Abdulkadir <adamj20@gmail.com>
2019-11-19 21:03:44 -0500

    First File

---
+++ Account.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Account extends Employee{
+  
+  
+  public Account(){
+    super();
+  }
+
+  public boolean Login(String username, String password){
+    String U;
+    String P;
+    int k=3;
+    System.out.println("Please enter Username and Password");
+    Scanner sc = new Scanner(System.in);
+    U = sc.nextLine();
+    //Scanner ss = new Scanner(System.in);
+    //P = ss.nextLine();
+    //for 
+    return false;
+  }
+}
